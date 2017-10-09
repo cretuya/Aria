0b1a3bfd7ef528bf3dd637df05c5f0835967c24c
Cherry Anne Retuya <cherryanne1011@gmail.com>
2017-10-09 15:24:51 +0800

    revised api

--- routes/api.php
+++ routes/api.php
@@ -17,42 +17,42 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('updateUserInfo', 'Api\UserController@updateUser');
+Route::post('updateUserInfo', 'Api\UserController@updateUser');
 
-Route::get('createband', 'Api\BandController@createBand');
+Route::post('createband', 'Api\BandController@createBand');
 Route::get('getBand', 'Api\BandController@getBand');
 // Route::get('updateBand', 'Api\BandController@updateBand');
 Route::get('editbandPic', 'Api\BandController@editBandPic');
 Route::get('searchForBandMember', 'Api\BandController@search');
-Route::get('addmember', 'Api\BandMemberController@addBandMember');
+Route::post('addmember', 'Api\BandMemberController@addBandMember');
 Route::get('deletemember', 'Api\BandMemberController@deleteBandMember');
 
 Route::get('videos', 'Api\VideoController@videos');
-Route::get('addVideo', 'Api\VideoController@addVideo');
+Route::post('addVideo', 'Api\VideoController@addVideo');
 Route::get('deleteVideo','Api\VideoController@deleteVideo');
 Route::get('editVideo', 'Api\VideoController@editVideo');
-Route::get('updateVideo', 'Api\VideoController@updateVideo');
+Route::post('updateVideo', 'Api\VideoController@updateVideo');
 
 
 Route::get('albums', 'Api\AlbumController@albums');
-Route::get('addAlbum', 'Api\AlbumController@addAlbum');
+Route::post('addAlbum', 'Api\AlbumController@addAlbum');
 Route::get('deleteAlbum','Api\AlbumController@deleteAlbum');
 Route::get('editAlbum', 'Api\AlbumController@editAlbum');
-Route::get('updateAlbum', 'Api\AlbumController@updateAlbum');
+Route::post('updateAlbum', 'Api\AlbumController@updateAlbum');
 
 
 Route::get('songs', 'Api\SongController@songs');
-Route::get('addSongs' , 'Api\SongController@addSongs');
+Route::post('addSongs' , 'Api\SongController@addSongs');
 Route::get('editSong', 'Api\SongController@editSong');
-Route::get('updateSong', 'Api\SongController@updateSong');
+Route::post('updateSong', 'Api\SongController@updateSong');
 Route::get('deleteSong', 'Api\SongController@deleteSong');
 
 Route::get('articles', 'Api\ArticleController@articles');
-Route::get('addArticle', 'Api\ArticleController@addArticle');
+Route::post('addArticle', 'Api\ArticleController@addArticle');
 Route::get('viewArticle', 'Api\ArticleController@viewArticle');
 Route::get('deleteArticle' , 'Api\ArticleController@deleteArticle');
 Route::get('editArticle', 'Api\ArticleController@editArticle');
-Route::get('updateArticle', 'Api\ArticleController@updateArticle');
+Route::post('updateArticle', 'Api\ArticleController@updateArticle');
 
 Route::get('genres', 'Api\AlbumController@genres');
 
