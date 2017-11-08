755d46538348bfbdf1e79c59c4bebe2def5cf250
Gil Ralph Canedo <gilralph.canedo@gmail.com>
2017-11-08 18:04:25 +0800

    added pl_id on preferences table

--- database/migrations/2017_07_27_021327_create_preferences_table.php
+++ database/migrations/2017_07_27_021327_create_preferences_table.php
@@ -16,8 +16,10 @@ class CreatePreferencesTable extends Migration
         Schema::create('preferences', function (Blueprint $table) {
             $table->string('user_id');
             $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->integer('pl_id')->unsigned()->nullable();
+            $table->foreign('pl_id')->references('pl_id')->on('playlists');
             $table->integer('band_id')->unsigned()->nullable();
-            $table->foreign('band_id')->references('band_id')->on('bands');        
+            $table->foreign('band_id')->references('band_id')->on('bands');       
             $table->timestamps();
             //nag usab ko diri
         });
